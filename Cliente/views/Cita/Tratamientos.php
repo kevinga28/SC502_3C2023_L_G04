0f128c3181173124731e4f2b1aac6b1606da7235
JuanD <110747940+Koyojsan@users.noreply.github.com>
2023-10-31 17:04:15 -0600

    Tratamientos hecho (Falta algunas funciones)

--- Cliente/views/Cita/Tratamientos.php
+++ Cliente/views/Cita/Tratamientos.php
@@ -15,7 +15,7 @@
     <!-- style css -->
     <link rel="stylesheet" href="../css/style.css">
     <!-- style css -->
-    <link rel="stylesheet" href="css/estilistaCita.css">
+    <link rel="stylesheet" href="css/tratamientosCita.css">
     <!-- Responsive-->
     <link rel="stylesheet" href="../css/responsive.css">
  
@@ -49,22 +49,24 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="cuadro-beige2">
+                    <div class="cuadro-beige2" style="padding-right: 100px; width: 650px;">
                         <h3><b>Tratamientos</b></h3>
                         <ul class="estilistasJD">
                             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                                <li class="container-tratamientos">
+                                <li class="container-tratamientos" style="display: block">
                                     <a href="#" class="nav-link">
-                                        <p id="sub-tratamientos" style="font-size: 20px; color: #593326">
+                                        <p id="sub-tratamientos-cabello" style="font-size: 20px; color: #593326; margin-bottom: 20px">
                                             Cabello
                                             <i class="fas fa-angle-left right"></i>
                                         </p>
                                     </a>
-                                    <ul class="nav nav-treeview" style="display: none;">
+                                    <ul class="nav nav-treeview-cabello" style="display: none;">
                                         <li class="nav-item">
                                             <p href="#" class="item-tratamiento">
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <a href="#">Largo Mujer</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
                                             </p>
                                         </li>
                                         <br>
@@ -72,6 +74,8 @@
                                             <p href="#" class="item-tratamiento">
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <a href="#">Corto Mujer</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
                                             </p>
                                         </li>
                                         <br>
@@ -79,6 +83,8 @@
                                             <p href="#" class="item-tratamiento">
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <a href="#">Largo + Lavado Mujer</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
                                             </p>
                                         </li>
                                         <br>
@@ -86,6 +92,8 @@
                                             <p href="#" class="item-tratamiento">
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <a href="#">Corto + Lavado Mujer</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
                                             </p>
                                         </li>
                                         <br>
@@ -93,6 +101,8 @@
                                             <p href="#" class="item-tratamiento">
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <a href="#">Largo Hombre</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
                                             </p>
                                         </li>
                                         <br>
@@ -100,6 +110,8 @@
                                             <p href="#" class="item-tratamiento">
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <a href="#">Corto Hombre</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
                                             </p>
                                         </li>
                                         <br>
@@ -107,6 +119,8 @@
                                             <p href="#" class="item-tratamiento">
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <a href="#">Largo + Lavado Hombre</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
                                             </p>
                                         </li>
                                         <br>
@@ -114,6 +128,8 @@
                                             <p href="#" class="item-tratamiento">
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <a href="#">Corto + Lavado Hombre</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
                                             </p>
                                         </li>
                                         <br>
@@ -121,6 +137,8 @@
                                             <p href="#" class="item-tratamiento">
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <a href="#">Niño - Niña</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
                                             </p>
                                         </li>
                                     </ul>
@@ -128,7 +146,84 @@
                             </ul>
                         </ul>
                         <div class="col_vacia"></div>
-                    </div>
+                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                                <li class="container-tratamientos" style="display: block">
+                                    <a href="#" class="nav-link">
+                                        <p id="sub-tratamientos-unas" style="font-size: 20px; color: #593326; margin-bottom: 20px">
+                                            Uñas
+                                            <i class="fas fa-angle-left right"></i>
+                                        </p>
+                                    </a>
+                                    <ul class="nav nav-treeview-unas" style="display: none;">
+                                        <li class="nav-item">
+                                            <p href="#" class="item-tratamiento">
+                                                <i class="far fa-circle nav-icon"></i>
+                                                <a href="#">Manicura</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
+                                            </p>
+                                        </li>
+                                        <br>
+                                        <li class="nav-item">
+                                            <p href="#" class="item-tratamiento">
+                                                <i class="far fa-circle nav-icon"></i>
+                                                <a href="#">Pedicura</a>
+                                                <div class="col_vacia"></div>
+                                                <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
+                                            </p>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <div class="col_vacia"></div>
+                            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                                    <li class="container-tratamientos" style="display: block">
+                                        <a href="#" class="nav-link">
+                                            <p id="sub-tratamientos-piel" style="font-size: 20px; color: #593326; margin-bottom: 20px">
+                                                Piel
+                                                <i class="fas fa-angle-left right"></i>
+                                            </p>
+                                        </a>
+                                        <ul class="nav nav-treeview-piel" style="display: none;">
+                                            <li class="nav-item">
+                                                <p href="#" class="item-tratamiento">
+                                                    <i class="far fa-circle nav-icon"></i>
+                                                    <a href="#">Eliminación de Manchas</a>
+                                                    <div class="col_vacia"></div>
+                                                    <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
+                                                </p>
+                                            </li>
+                                            <br>
+                                            <li class="nav-item">
+                                                <p href="#" class="item-tratamiento">
+                                                    <i class="far fa-circle nav-icon"></i>
+                                                    <a href="#">Tratamiento de acne</a>
+                                                    <div class="col_vacia"></div>
+                                                    <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
+                                                </p>
+                                            </li>
+                                            <br>
+                                            <li class="nav-item">
+                                                <p href="#" class="item-tratamiento">
+                                                    <i class="far fa-circle nav-icon"></i>
+                                                    <a href="#">Antiaging Facial</a>
+                                                    <div class="col_vacia"></div>
+                                                    <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
+                                                </p>
+                                            </li>
+                                            <br>
+                                            <li class="nav-item">
+                                                <p href="#" class="item-tratamiento">
+                                                    <i class="far fa-circle nav-icon"></i>
+                                                    <a href="#">Diagnostico Facial</a>
+                                                    <div class="col_vacia"></div>
+                                                    <a href="#" class="precio-tratamiento" style="font-size: 20px">$5000 IVI</a>
+                                                </p>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                        </div>
                     <div class="titulo">
                         <a href="#"><img src="../images/logo.png" alt="#" class="imag_medio" /></a>
                     </div>
@@ -153,20 +248,68 @@
     <!-- final footer -->
 
     <script>
-    document.addEventListener("DOMContentLoaded", function() {
+        document.addEventListener("DOMContentLoaded", function() {
+            // Selecciona el "Cabello" por su ID
+            const cabelloLink = document.getElementById("sub-tratamientos");
+
+            // Selecciona el menú desplegable de la clase
+            const menuCabello = document.querySelector(".nav.nav-treeview");
+
+            // Agrega un controlador a "Cabello"
+            cabelloLink.addEventListener("click", function() {
+                // Cambia como se ve el menú desplegable
+                menuCabello.style.display = (menuCabello.style.display === "none") ? "block" : "none";
+
+                // Cambia el ícono para que apunte hacia al lado cuando el menú se cierra
+                const icon = cabelloLink.querySelector("i");
+                if (menuCabello.style.display === "none") {
+                    icon.classList.remove("fa-angle-down");
+                    icon.classList.add("fa-angle-left");
+                } else {
+                    icon.classList.remove("fa-angle-left");
+                    icon.classList.add("fa-angle-down"); 
+                    // Cambia el ícono para que apunte hacia abajo cuando se abra el menu
+                }
+            });
+        });
+
+        document.addEventListener("DOMContentLoaded", function() {
         // Selecciona el enlace "Cabello" por su ID
-        const cabelloLink = document.getElementById("sub-tratamientos");
+        const cabelloLink = document.getElementById("sub-tratamientos-cabello");
+        const unasLink = document.getElementById("sub-tratamientos-unas");
+        const pielLink = document.getElementById("sub-tratamientos-piel");
 
         // Selecciona el menú desplegable por su clase
-        const menuCabello = document.querySelector(".nav.nav-treeview");
+        const menuCabello = document.querySelector(".nav.nav-treeview-cabello");
+        const menuUnas = document.querySelector(".nav.nav-treeview-unas");
+        const menuPiel = document.querySelector(".nav.nav-treeview-piel");
 
-        // Agrega un controlador de eventos al enlace "Cabello"
-        cabelloLink.addEventListener("click", function() {
-            // Cambia la visibilidad del menú desplegable
-            menuCabello.style.display = (menuCabello.style.display === "none") ? "block" : "none";
-        });
+        // Función para gestionar el clic en los enlaces
+        function handleLinkClick(link, menu) {
+            link.addEventListener("click", function() {
+                // Cambia la visibilidad del menú desplegable
+                menu.style.display = (menu.style.display === "none") ? "block" : "none";
+
+                // Cambia la clase del ícono para que apunte hacia al lado cuando el menú se cierra
+                const icon = link.querySelector("i");
+                if (menu.style.display === "none") {
+                    icon.classList.remove("fa-angle-down");
+                    icon.classList.add("fa-angle-left");
+                } else {
+                    icon.classList.remove("fa-angle-left");
+                    icon.classList.add("fa-angle-down");
+                }
+            });
+        }
+
+        // Aplica la funcionalidad a los enlaces "Cabello", "Uñas" y "Piel"
+        handleLinkClick(cabelloLink, menuCabello);
+        handleLinkClick(unasLink, menuUnas);
+        handleLinkClick(pielLink, menuPiel);
     });
-</script>
+
+    </script>
+
 
 
 </body>
