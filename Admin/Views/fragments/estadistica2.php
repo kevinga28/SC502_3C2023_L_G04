0621b829c07a75c6c1d7a5b8847bd945f4f6bdce
JsoNJ552 <114025870+JsoNJ552@users.noreply.github.com>
2023-12-07 04:30:41 -0600

    reportes producotos, falta filtro por checkbox

--- Admin/Views/fragments/estadistica2.php
+++ Admin/Views/fragments/estadistica2.php
@@ -61,7 +61,7 @@
 
 <div class="col-md-4">
     <?php
-  
+
     // Obtener el tipo de reporte seleccionado
     $tipoReporte = isset($_GET['tipo']) ? $_GET['tipo'] : 'semana'; // Si no se proporciona un tipo, se asume 'semana'
 
@@ -173,7 +173,7 @@
 
 
 
-<div class="card">
+    <div class="card">
         <div class="card-header border-0">
             <div class="card-tools">
                 <div class="btn-group ml-4">
@@ -194,17 +194,241 @@
         <div class="card-body">
             <div class="d-flex">
                 <p class="d-flex flex-column">
-                    
+
                 </p>
 
             </div>
 
             <div class="position-relative mb-4">
-            <canvas id="rentabilidad-tratamientos" height="170px"></canvas>
+                <canvas id="rentabilidad-tratamientos" height="170px"></canvas>
             </div>
 
         </div>
     </div>
 </div>
 
+<div class="col-md-4">
+    <?php
+    // Obtener la lista de productos
+    $query_productos = "SELECT p.Codigo, p.nombre
+    FROM producto p
+    INNER JOIN detalle_factura df ON p.Codigo = df.CodigoProducto";
 
+    $stmt_productos = $conexion->prepare($query_productos);
+    $stmt_productos->execute();
+
+    $productos = $stmt_productos->fetchAll(PDO::FETCH_ASSOC);
+
+    // Obtener el producto seleccionado (por defecto, todos)
+    $productoSeleccionado = isset($_GET['producto']) ? $_GET['producto'] : 'todos';
+
+    // Construir la consulta según la selección del usuario
+    if ($productoSeleccionado === 'todos') {
+        $query_ventas_productos = "
+            SELECT p.nombre AS nombre_producto, SUM(df.Cantidad) AS cantidad_vendida
+            FROM producto p
+            INNER JOIN detalle_factura df ON p.Codigo = df.CodigoProducto
+            INNER JOIN factura f ON df.IdFactura = f.IdFactura
+            GROUP BY p.Codigo
+        ";
+    } else {
+        $query_ventas_productos = "
+            SELECT p.nombre AS nombre_producto, SUM(df.Cantidad) AS cantidad_vendida
+            FROM producto p
+            INNER JOIN detalle_factura df ON p.Codigo = df.CodigoProducto
+            INNER JOIN factura f ON df.IdFactura = f.IdFactura
+            WHERE p.Codigo = :producto
+            GROUP BY p.Codigo
+        ";
+    }
+
+    $stmt_ventas_productos = $conexion->prepare($query_ventas_productos);
+
+    // Bind para el parámetro :producto si no es 'todos'
+    if ($productoSeleccionado !== 'todos') {
+        $stmt_ventas_productos->bindParam(':producto', $productoSeleccionado, PDO::PARAM_INT);
+    }
+
+    $stmt_ventas_productos->execute();
+
+    $labels_productos = [];
+    $data_ventas_productos = [];
+
+    if ($stmt_ventas_productos->rowCount() > 0) {
+        while ($row = $stmt_ventas_productos->fetch(PDO::FETCH_ASSOC)) {
+            $labels_productos[] = $row['nombre_producto'];
+            $data_ventas_productos[] = $row['cantidad_vendida'];
+        }
+    }
+    ?>
+
+    <div class="card">
+        <div class="card-header border-0">
+            <div class="card-tools">
+                <div class="btn-group ml-4">
+                    <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52" aria-expanded="true">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <div class="dropdown-menu" role="menu" x-placement="bottom-start">
+                        <a href="?tipo=semana" class="dropdown-item">Semana</a>
+                        <a href="?tipo=mes" class="dropdown-item">Mes</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between">
+                <h3 class="card-title">Productos Vendidos</h3>
+                <a href="javascript:void(0);">Ver Reporte</a>
+            </div>
+        </div>
+        <div class="card-body">
+
+            <form method="GET" action="">
+                <label for="producto">Seleccione un producto:</label>
+                <div class="input-group mb-3">
+                    <select class="custom-select" name="producto" id="producto">
+                        <option value="todos">Todos</option>
+                        <?php foreach ($productos as $producto) : ?>
+                            <option value="<?php echo $producto['Codigo']; ?>" <?php echo ($productoSeleccionado == $producto['Codigo']) ? 'selected' : ''; ?>>
+                                <?php echo $producto['nombre']; ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                    <div class="input-group-append">
+                        <label class="input-group-text" for="producto">Producto</label>
+                    </div>
+                </div>
+
+            </form>
+
+            <div class="position-relative mb-4">
+                <canvas id="productos-vendidos" height="170px"></canvas>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-md-4">
+    <?php
+    // Obtener los rangos de precio seleccionados
+    $rangosPrecio = isset($_GET['rangoPrecio']) ? $_GET['rangoPrecio'] : [];
+
+    // Construir la parte de la consulta según los rangos seleccionados
+    $whereRangoPrecio = '';
+    if (!empty($rangosPrecio)) {
+        $rangosCondicion = [];
+        foreach ($rangosPrecio as $rango) {
+            if ($rango === '12000-inf') {
+                $rangosCondicion[] = "(t.precio >= 12000)";
+            } else {
+                list($min, $max) = explode('-', $rango);
+                $rangosCondicion[] = "(t.precio BETWEEN $min AND $max)";
+            }
+        }
+        $whereRangoPrecio = ' AND (' . implode(' OR ', $rangosCondicion) . ')';
+    }
+
+    // Consulta para obtener los tratamientos filtrados por rango de precio
+    $query_tratamientos_filtrados = "
+        SELECT 
+            CASE
+                WHEN t.precio BETWEEN 1000 AND 3000 THEN '1000-3000'
+                WHEN t.precio BETWEEN 3000 AND 7000 THEN '3000-7000'
+                WHEN t.precio BETWEEN 7000 AND 12000 THEN '7000-12000'
+                WHEN t.precio >= 12000 THEN '12000-inf'
+            END AS rango_precio,
+            COUNT(ct.IdCita) AS cantidad_vendida
+        FROM tratamiento t
+        LEFT JOIN cita_tratamiento ct ON t.IdTratamiento = ct.IdTratamiento
+        LEFT JOIN cita c ON ct.IdCita = c.IdCita
+        WHERE 1 $whereRangoPrecio
+        GROUP BY rango_precio
+    ";
+
+    $stmt_tratamientos_filtrados = $conexion->prepare($query_tratamientos_filtrados);
+    $stmt_tratamientos_filtrados->execute();
+
+    $labels_tratamientos_filtrados = [];
+    $data_tratamientos_filtrados = [];
+
+    if ($stmt_tratamientos_filtrados->rowCount() > 0) {
+        while ($row = $stmt_tratamientos_filtrados->fetch(PDO::FETCH_ASSOC)) {
+            $labels_tratamientos_filtrados[] = $row['rango_precio'];
+            $data_tratamientos_filtrados[] = $row['cantidad_vendida'];
+        }
+    }
+    ?>
+    <div class="card">
+        <div class="card-header border-0">
+            <div class="card-tools">
+                <div class="btn-group ml-4">
+                    <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52" aria-expanded="true">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <div class="dropdown-menu" role="menu" x-placement="bottom-start">
+                        <a href="?tipo=semana" class="dropdown-item">Semana</a>
+                        <a href="?tipo=mes" class="dropdown-item">Mes</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between">
+                <h3 class="card-title">Tratamientos Vendidos</h3>
+                <a href="javascript:void(0);" id="ver-reporte">Ver Reporte</a>
+            </div>
+        </div>
+        <div class="card-body">
+            <label class="checkbox-container">1000 - 3000
+                <input type="checkbox" name="rangoPrecio[]" value="1000-3000">
+                <span class="checkmark"></span>
+            </label>
+            <label class="checkbox-container">3000 - 7000
+                <input type="checkbox" name="rangoPrecio[]" value="3000-7000">
+                <span class="checkmark"></span>
+            </label>
+            <label class="checkbox-container">7000 - 12000
+                <input type="checkbox" name="rangoPrecio[]" value="7000-12000">
+                <span class="checkmark"></span>
+            </label>
+            <label class="checkbox-container">12000 en adelante
+                <input type="checkbox" name="rangoPrecio[]" value="12000-inf">
+                <span class="checkmark"></span>
+            </label>
+        </div>
+
+        <div class="position-relative mb-4">
+            <canvas id="tratamientos-vendidos-filtrados" height="170px"></canvas>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    $(document).ready(function () {
+        // Función para obtener los datos y actualizar el gráfico
+        function updateChart() {
+            $.ajax({
+                url: window.location.href,
+                method: "GET",
+                data: { rangoPrecio: $('input[name="rangoPrecio[]"]:checked').map(function () { return this.value; }).get() },
+                success: function (data) {
+                    var newData = JSON.parse(data);
+
+                    if (newData.labels && newData.data) {
+                        myChart.data.labels = newData.labels;
+                        myChart.data.datasets[0].data = newData.data;
+                        myChart.update();
+                    } else {
+                        console.error("Error en el formato de los datos recibidos.");
+                    }
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.error("Error en la solicitud AJAX:", textStatus, errorThrown);
+                }
+            });
+        }
+
+        // Llamamos a la función al cargar la página
+        updateChart();
+
+        // Configuramos el evento change para los checkboxes
+        $('input[name="rangoPrecio[]"]').on('change', updateChart);
+    });
+</script>
--- Admin/Views/index.php
+++ Admin/Views/index.php
@@ -28,25 +28,25 @@ $rol = $authController->obtenerRolUsuario();
 
 // Verificar si el usuario está autenticado
 if (!isset($_SESSION['cedula'])) {
-  header("Location: ../views/logueo/logueo.php");
-  exit();
+    header("Location: ../views/logueo/logueo.php");
+    exit();
 }
 
 
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["action"]) && $_GET["action"] == "logout") {
-  // Destruir todas las variables de sesión
-  $_SESSION = array();
+    // Destruir todas las variables de sesión
+    $_SESSION = array();
 
-  if (isset($_COOKIE[session_name()])) {
-    setcookie(session_name(), '', time() - 42000, '/');
-  }
+    if (isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time() - 42000, '/');
+    }
 
-  // Destruir la sesión
-  session_destroy();
+    // Destruir la sesión
+    session_destroy();
 
-  // Redirigir a la página de inicio de sesión
-  header("Location: ../views/logueo/logueo.php");
-  exit();
+    // Redirigir a la página de inicio de sesión
+    header("Location: ../views/logueo/logueo.php");
+    exit();
 }
 
 ?>
@@ -56,37 +56,37 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["action"]) && $_GET["acti
 <html lang="es">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Evolve | Inicio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Evolve | Inicio</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  <link rel="stylesheet" href="dist/css/style.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
 
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
-  <div class="wrapper">
+<div class="wrapper">
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand ">
-      <?php
-      include 'fragments/navbar.php'
-      ?>
+        <?php
+        include 'fragments/navbar.php'
+        ?>
     </nav>
     <!-- /.navbar -->
 
@@ -94,209 +94,209 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["action"]) && $_GET["acti
     <aside class="main-sidebar elevation-4 color-custom">
 
 
-      <?php
-      include 'fragments/aside.php'
-      ?>
+        <?php
+        include 'fragments/aside.php'
+        ?>
     </aside>
 
 
     <div class="content-wrapper">
 
-      <div class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1 class="m-0">Sistema Evolve</h1>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Sistema Evolve</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+                            <li class="breadcrumb-item active">General</li>
+                        </ol>
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Inicio</a></li>
-                <li class="breadcrumb-item active">General</li>
-              </ol>
+        </div>
+
+
+
+        <section class="content">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <?php
+                    include 'fragments/smallBox.php'
+                    ?>
+
+                </div>
+
+                <div class="row">
+
+                    <section class="col-lg-9 connectedSortable">
+                        <div class="row">
+                            <?php
+                            include 'fragments/estadistica.php'
+                            ?>
+                        </div>
+
+                    </section>
+
+                    <section class="col-lg-3 connectedSortable">
+
+                        <div class="card">
+                            <?php
+                            include 'fragments/listaInventario.php'
+                            ?>
+                        </div>
+
+                    </section>
+
+                    <section class="col-lg-12 connectedSortable">
+                        <div class="row">
+                            <?php
+                            include 'fragments/estadistica2.php'
+                            ?>
+                        </div>
+
+                    </section>
+
+
+                    <section class="col-lg-12 connectedSortable">
+
+                        <div class="card">
+                            <?php
+                            include 'fragments/calendarioIndex.php'
+                            ?>
+                        </div>
+
+                    </section>
+
+
+
+                </div>
+
             </div>
-          </div>
-        </div>
-      </div>
-
-
-
-      <section class="content">
-        <div class="container-fluid">
-
-          <div class="row">
-            <?php
-            include 'fragments/smallBox.php'
-            ?>
-
-          </div>
-
-          <div class="row">
-
-            <section class="col-lg-9 connectedSortable">
-              <div class="row">
-                <?php
-                include 'fragments/estadistica.php'
-                ?>
-              </div>
-
-            </section>
-
-            <section class="col-lg-3 connectedSortable">
-
-              <div class="card">
-                <?php
-                include 'fragments/listaInventario.php'
-                ?>
-              </div>
-
-            </section>
-
-            <section class="col-lg-12 connectedSortable">
-              <div class="row">
-                <?php
-                include 'fragments/estadistica2.php'
-                ?>
-              </div>
-
-            </section>
-
-
-            <section class="col-lg-12 connectedSortable">
-
-              <div class="card">
-                <?php
-                include 'fragments/calendarioIndex.php'
-                ?>
-              </div>
-
-            </section>
-
-
-
-          </div>
-
-        </div>
-      </section>
+        </section>
     </div>
 
     <footer class="main-footer no-print">
-      <?php
-      include 'fragments/footer.php'
-      ?>
+        <?php
+        include 'fragments/footer.php'
+        ?>
     </footer>
 
-  </div>
+</div>
 
 
-  <!-- jQuery -->
-  <script src="plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.min.js"></script>
-  <!-- SWEETALERT -->
-  <script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- SWEETALERT -->
+<script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>
 
-  <script src="plugins/select2/js/select2.full.min.js"></script>
-  <script src="logueo/js/logout.js"></script>
-  <!-- ChartJS -->
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  <!-- Estadistica -->
-  
-  <!-- Script 1: Gráfico de Ventas y Citas -->
-  <script>
+<script src="plugins/select2/js/select2.full.min.js"></script>
+<script src="logueo/js/logout.js"></script>
+<!-- ChartJS -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<!-- Estadistica -->
+
+<!-- Script 1: Gráfico de Ventas y Citas -->
+<script>
     var ctx = document.getElementById('myChart');
     var data = {
-      labels: ['Productos Vendidos', 'Citas Realizadas'],
-      datasets: [{
-        label: 'Ventas y Citas',
-        data: [<?php echo $cantidadProductos; ?>, <?php echo $cantidadCitas; ?>],
-        backgroundColor: [
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 99, 132, 0.2)'
-        ],
-        borderColor: [
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 99, 132, 1)'
-        ],
-        borderWidth: 1
-      }]
+        labels: ['Productos Vendidos', 'Citas Realizadas'],
+        datasets: [{
+            label: 'Ventas y Citas',
+            data: [<?php echo $cantidadProductos; ?>, <?php echo $cantidadCitas; ?>],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1
+        }]
     };
     const config = {
-      type: 'line',
-      data: data,
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
+        type: 'line',
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
         }
-      }
     };
     new Chart(ctx, config);
-  </script>
+</script>
 
-  <!-- Script 2: Gráfico de Citas por Día -->
-  <script>
+<!-- Script 2: Gráfico de Citas por Día -->
+<script>
     var labels = <?php echo json_encode($labels); ?>;
     var data = <?php echo json_encode($data); ?>;
     var ctx1 = document.getElementById('total-citas').getContext('2d');
     var myChart1 = new Chart(ctx1, {
-      type: 'line',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Citas por Día',
-          data: data,
-          fill: false,
-          borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1
-        }]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: {
-            beginAtZero: true
-          }
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Citas por Día',
+                data: data,
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
         }
-      }
     });
-  </script>
+</script>
 
-  <!-- Script 3: Gráfico de Reporte de Facturas -->
-  <script>
+<!-- Script 3: Gráfico de Reporte de Facturas -->
+<script>
     var labels = <?php echo json_encode($labels); ?>;
     var data = <?php echo json_encode($data); ?>;
     var ctx2 = document.getElementById('reporte-ventas').getContext('2d');
     var myChart2 = new Chart(ctx2, {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Reporte de Facturas',
-          data: data,
-          backgroundColor: [
-            'rgba(0,123,255,0.8)',
-            'rgba(255,193,7,0.8)',
-          ],
-          borderColor: [
-            'rgb(0,123,255)',
-            'rgb(255,193,7)',
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: {
-            beginAtZero: true
-          }
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Reporte de Facturas',
+                data: data,
+                backgroundColor: [
+                    'rgba(0,123,255,0.8)',
+                    'rgba(255,193,7,0.8)',
+                ],
+                borderColor: [
+                    'rgb(0,123,255)',
+                    'rgb(255,193,7)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
         }
-      }
     });
-  </script>
+</script>
 
 
 <!-- Script 4: Gráfico de Rentabilidad de Tratamientos -->
@@ -311,12 +311,12 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["action"]) && $_GET["acti
         data: {
             labels: labels4,
             datasets: [{
-                    label: 'Ingresos',
-                    data: ingresos4,
-                    backgroundColor: 'rgba(75, 192, 192, 0.8)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                },
+                label: 'Ingresos',
+                data: ingresos4,
+                backgroundColor: 'rgba(75, 192, 192, 0.8)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            },
                 {
                     label: 'Costos',
                     data: citas4,
@@ -344,7 +344,60 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["action"]) && $_GET["acti
     });
 </script>
 
+<script>
+    var labels_productos = <?php echo json_encode($labels_productos); ?>;
+    var data_ventas_productos = <?php echo json_encode($data_ventas_productos); ?>;
+    var ctx_productos = document.getElementById('productos-vendidos').getContext('2d');
+    var myChart_productos = new Chart(ctx_productos, {
+        type: 'bar',
+        data: {
+            labels: labels_productos,
+            datasets: [{
+                label: 'Productos Vendidos',
+                data: data_ventas_productos,
+                backgroundColor: 'rgba(75, 192, 192, 0.8)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
 
+<!-- Script para el gráfico de tratamientos filtrados -->
+<script>
+    var labels_tratamientos_filtrados = <?php echo json_encode($labels_tratamientos_filtrados); ?>;
+    var data_tratamientos_filtrados = <?php echo json_encode($data_tratamientos_filtrados); ?>;
+    var ctx_tratamientos_filtrados = document.getElementById('tratamientos-vendidos-filtrados').getContext('2d');
+    var myChart_tratamientos_filtrados = new Chart(ctx_tratamientos_filtrados, {
+        type: 'bar',
+        data: {
+            labels: labels_tratamientos_filtrados,
+            datasets: [{
+                label: 'Tratamientos Vendidos',
+                data: data_tratamientos_filtrados,
+                backgroundColor: 'rgba(75, 192, 192, 0.8)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
 </body>
 
 </html>
