ca618a2102ec3f8053b00395d1c7d44fea3d00c1
kevinga28 <kevinga2806@gmail.com>
2023-12-08 17:07:31 -0600

    Grafico para productos la cantidad vendidos,para jason

--- Admin/Views/fragments/estadistica2.php
+++ Admin/Views/fragments/estadistica2.php
@@ -206,13 +206,12 @@
         </div>
     </div>
 </div>
-
 <div class="col-md-4">
     <?php
     // Obtener la lista de productos
     $query_productos = "SELECT p.Codigo, p.nombre
-    FROM producto p
-    INNER JOIN detalle_factura df ON p.Codigo = df.CodigoProducto";
+        FROM producto p
+        INNER JOIN detalle_factura df ON p.Codigo = df.CodigoProducto";
 
     $stmt_productos = $conexion->prepare($query_productos);
     $stmt_productos->execute();
@@ -281,23 +280,22 @@
             </div>
         </div>
         <div class="card-body">
-        
-            <form method="GET" action="">
+            <form method="GET" action="" id="formActualizarGrafico" class="update-chart-form">
                 <label for="producto">Seleccione un producto:</label>
                 <div class="input-group mb-3">
-    <select class="custom-select" name="producto" id="producto">
-        <option value="todos">Todos</option>
-        <?php foreach ($productos as $producto) : ?>
-            <option value="<?php echo $producto['Codigo']; ?>" <?php echo ($productoSeleccionado == $producto['Codigo']) ? 'selected' : ''; ?>>
-                <?php echo $producto['nombre']; ?>
-            </option>
-        <?php endforeach; ?>
-    </select>
-    <div class="input-group-append">
-        <label class="input-group-text" for="producto">Producto</label>
-    </div>
-</div>
-
+                    <select class="custom-select" name="producto" id="producto">
+                        <option value="todos">Todos</option>
+                        <?php foreach ($productos as $producto) : ?>
+                            <option value="<?php echo $producto['Codigo']; ?>" <?php echo ($productoSeleccionado == $producto['Codigo']) ? 'selected' : ''; ?>>
+                                <?php echo $producto['nombre']; ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                    <div class="input-group-append">
+                        <label class="input-group-text" for="producto">Producto</label>
+                    </div>
+                    <button type="button" class="btn btn-primary" id="btnActualizarGrafico">Actualizar Gráfico</button>
+                </div>
             </form>
 
             <div class="position-relative mb-4">
@@ -307,92 +305,46 @@
     </div>
 </div>
 
-<div class="col-md-4">
-    <?php
-    // Obtener los rangos de precio seleccionados
-    $rangosPrecio = isset($_GET['rangoPrecio']) ? $_GET['rangoPrecio'] : [];
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    $(document).ready(function () {
+    // Función para obtener los datos y actualizar el gráfico de productos
+    function updateProductChart() {
+        // Obtener el valor seleccionado del producto
+        var productoSeleccionado = $('#producto').val();
 
-    // Construir la parte de la consulta según los rangos seleccionados
-    $whereRangoPrecio = '';
-    if (!empty($rangosPrecio)) {
-        $rangosCondicion = [];
-        foreach ($rangosPrecio as $rango) {
-            if ($rango === '12000-inf') {
-                $rangosCondicion[] = "(t.precio >= 12000)";
-            } else {
-                list($min, $max) = explode('-', $rango);
-                $rangosCondicion[] = "(t.precio BETWEEN $min AND $max)";
+        // Realizar la solicitud AJAX
+        $.ajax({
+            url: 'estadistica2.php',
+            method: 'GET',
+            data: { producto: productoSeleccionado },
+            success: function (data) {
+                console.log("Datos recibidos:", data); // Agrega esta línea para registrar los datos recibidos
+                var newData = JSON.parse(data);
+
+                if (newData.labels && newData.data) {
+                    // Actualizar los datos del gráfico de productos
+                    myChart_productos.data.labels = newData.labels;
+                    myChart_productos.data.datasets[0].data = newData.data;
+                    myChart_productos.update();
+                } else {
+                    console.error("Error en el formato de los datos recibidos.");
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.error("Error en la solicitud AJAX:", textStatus, errorThrown);
             }
-        }
-        $whereRangoPrecio = ' AND (' . implode(' OR ', $rangosCondicion) . ')';
+        });
     }
 
-    // Consulta para obtener los tratamientos filtrados por rango de precio
-    $query_tratamientos_filtrados = "
-        SELECT t.nombre AS nombre_tratamiento, COUNT(ct.IdCita) AS cantidad_vendida
-        FROM tratamiento t
-        LEFT JOIN cita_tratamiento ct ON t.IdTratamiento = ct.IdTratamiento
-        LEFT JOIN cita c ON ct.IdCita = c.IdCita
-        WHERE 1 $whereRangoPrecio
-        GROUP BY t.nombre
-    ";
+    // Configuramos el evento click para el botón de actualizar gráfico de productos
+    $('#btnActualizarGrafico').click(function () {
+        updateProductChart();
+    });
 
-    $stmt_tratamientos_filtrados = $conexion->prepare($query_tratamientos_filtrados);
-    $stmt_tratamientos_filtrados->execute();
+    // Llamamos a la función al cargar la página
+    updateProductChart();
+});
 
-    $labels_tratamientos_filtrados = [];
-    $data_tratamientos_filtrados = [];
-
-    if ($stmt_tratamientos_filtrados->rowCount() > 0) {
-        while ($row = $stmt_tratamientos_filtrados->fetch(PDO::FETCH_ASSOC)) {
-            $labels_tratamientos_filtrados[] = $row['nombre_tratamiento'];
-            $data_tratamientos_filtrados[] = $row['cantidad_vendida'];
-        }
-    }
-    ?>
-<div class="card">
-        <div class="card-header border-0">
-            <div class="card-tools">
-                <div class="btn-group ml-4">
-                    <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52" aria-expanded="true">
-                        <i class="fas fa-bars"></i>
-                    </button>
-                    <div class="dropdown-menu" role="menu" x-placement="bottom-start">
-                        <a href="?tipo=semana" class="dropdown-item">Semana</a>
-                        <a href="?tipo=mes" class="dropdown-item">Mes</a>
-                    </div>
-                </div>
-            </div>
-            <div class="d-flex justify-content-between">
-                <h3 class="card-title">Tratamientos Vendidos</h3>
-                <a href="javascript:void(0);">Ver Reporte</a>
-            </div>
-        </div>
-        <div class="card-body">
-        <label class="checkbox-container">1000 - 3000
-                        <input type="checkbox" name="rangoPrecio[]" value="1000-3000">
-                        <span class="checkmark"></span>
-                    </label>
-                    <label class="checkbox-container">3000 - 7000
-                        <input type="checkbox" name="rangoPrecio[]" value="3000-7000">
-                        <span class="checkmark"></span>
-                    </label>
-                    <label class="checkbox-container">7000 - 12000
-                        <input type="checkbox" name="rangoPrecio[]" value="7000-12000">
-                        <span class="checkmark"></span>
-                    </label>
-                    <label class="checkbox-container">12000 en adelante
-                        <input type="checkbox" name="rangoPrecio[]" value="12000-inf">
-                        <span class="checkmark"></span>
-                    </label>
-</div>
-
-            </form>
-
-            <div class="position-relative mb-4">
-                <canvas id="tratamientos-vendidos-filtrados" height="170px"></canvas>
-            </div>
-        </div>
-    </div>
-</div>
-
+</script>
--- Admin/Views/index.php
+++ Admin/Views/index.php
@@ -344,32 +344,10 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["action"]) && $_GET["acti
     });
 </script>
 
-<script>
-    var labels_productos = <?php echo json_encode($labels_productos); ?>;
-    var data_ventas_productos = <?php echo json_encode($data_ventas_productos); ?>;
-    var ctx_productos = document.getElementById('productos-vendidos').getContext('2d');
-    var myChart_productos = new Chart(ctx_productos, {
-        type: 'bar',
-        data: {
-            labels: labels_productos,
-            datasets: [{
-                label: 'Productos Vendidos',
-                data: data_ventas_productos,
-                backgroundColor: 'rgba(75, 192, 192, 0.8)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: true,
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-</script>
+
+
+
+
 
 <!-- Script para el gráfico de tratamientos filtrados -->
 <script>
